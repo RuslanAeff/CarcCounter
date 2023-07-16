b7f543d856fa259202cff44e4bb695607f6df187
RuslanAeff <135730667+RuslanAeff@users.noreply.github.com>
2023-07-17 01:19:36 +0400

    Delete CarcCounter.cs

--- CarcCounter.cs
+++
@@ -1,5 +0,0 @@
-﻿Build started...
-1>------ Build started: Project: CarcCounter, Configuration: Debug Any CPU ------
-1>  CarcCounter -> C:\Users\RUSLAN'PC\source\repos\CarcCounter\bin\Debug\CarcCounter.exe
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
-========== Build started at 1:07 AM and took 00,262 seconds ==========
